fa811748c0d7481a5b81fc5699b81f0b30fd14a8
leigh966 <leigh966@hotmail.com>
2023-10-15 14:29:41 +0100

    first commit

---
+++ DarkSoulsSaver/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DarkSoulsSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(519, 274);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(537, 51);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(196, 33);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Backup";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtDescription);
+            this.groupBox1.Location = new System.Drawing.Point(537, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(196, 183);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create Backup";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(37, 128);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(126, 49);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(6, 37);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(184, 76);
+            this.txtDescription.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(537, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(196, 33);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Backup";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 302);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button btnLoad;
+        private GroupBox groupBox1;
+        private Button btnCreate;
+        private Label label1;
+        private TextBox txtDescription;
+        private Button btnDelete;
+    }
+}
---
+++ DarkSoulsSaver/Form1.cs
@@ -0,0 +1,173 @@
+namespace DarkSoulsSaver
+{
+    public partial class Form1 : Form
+    {
+
+        private string saveLocation;
+        private string backupLocation;
+        private string currentGame;
+        private string[] backupDirectories;
+        private const string BACKUP_INFO_FILENAME = "backupInfo.csv";
+        private const int LISTBOX_COLUMN_PADDING = 30;
+
+        public Form1()
+        {
+
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string myDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            currentGame = "DarkSoulsIII";
+            saveLocation = Path.Combine(appData, currentGame);
+            string dssBase = Path.Combine(myDocs, "DarkSoulsSaver");
+            backupLocation = Path.Combine(dssBase, currentGame);
+
+            Directory.CreateDirectory(backupLocation);
+            InitializeComponent();
+
+        }
+
+        private void AddListBoxRow(string date, string time, string desc)
+        {
+            listBox1.Items.Add
+                (
+                date.PadRight(LISTBOX_COLUMN_PADDING - date.Length, ' ')
+                + time.PadRight(LISTBOX_COLUMN_PADDING - time.Length, ' ')
+                + desc.Replace("*comma*", ",")
+                ) ;
+        }
+
+        private void AddToListBox(string dir)
+        {
+            string dataFile = Path.Combine(dir, BACKUP_INFO_FILENAME);
+            string text = File.ReadAllText(dataFile);
+            string outputString = "";
+            string[] fields = text.Split(",");
+            AddListBoxRow(fields[0], fields[1], fields[2]);
+        }
+
+        private void RefreshControls()
+        {
+            listBox1.Items.Clear();
+            AddListBoxRow("Date", "Time", "Description");
+            backupDirectories = Directory.GetDirectories(backupLocation);
+            foreach (string dir in backupDirectories)
+            {
+                AddToListBox(dir);
+            }
+            btnLoad.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshControls();
+        }
+
+        static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
+
+        private void WriteBackupInfo(string directory, string description)
+        {
+            string backupInfoFile = Path.Combine(directory, BACKUP_INFO_FILENAME);
+            File.WriteAllText(backupInfoFile, String.Format("{0},{1},{2}", DateTime.Now.ToString("dd/MM/yyyy"), DateTime.Now.ToString("HH:mm"), description));
+        }
+
+        private void CreateBackup(string description)
+        {
+            description = description.Replace(",", "*comma*");
+            string id = Guid.NewGuid().ToString();
+            Directory.CreateDirectory(Path.Combine(backupLocation, id));
+            string toDirectory = Path.Combine(backupLocation, id);
+            CopyDirectory(saveLocation, toDirectory, true);
+            WriteBackupInfo(toDirectory, description);
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            CreateBackup(txtDescription.Text);
+            txtDescription.Text = "";
+            RefreshControls();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Directory.Delete(GetPathToSelectedBackup(), true);
+            RefreshControls();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool enabled = listBox1.SelectedIndex > 0;
+            btnDelete.Enabled = enabled;
+            btnLoad.Enabled = enabled;
+
+        }
+
+        private void AskIfBackupFirst()
+        {
+            DialogResult confirmResult = MessageBox.Show("Would you like to backup your saves first?",
+                         "Backup?",
+                         MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                CreateBackup("Pre-Load Backup");
+            }
+
+        }
+
+        private bool ConfirmLoad()
+        {
+            DialogResult confirmResult = MessageBox.Show("This action will overwite your implemented saves with those from the backup!", "Confirm Overwrite!!", MessageBoxButtons.OKCancel);
+
+            return confirmResult == DialogResult.OK;
+
+        }
+
+        private string GetPathToSelectedBackup()
+        {
+            return backupDirectories[listBox1.SelectedIndex - 1];
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            AskIfBackupFirst();
+            if(ConfirmLoad())
+            {
+                Directory.Delete(saveLocation, true);
+                CopyDirectory(GetPathToSelectedBackup(), saveLocation, true);
+            }
+            RefreshControls();
+        }
+    }
+}
