2ac6346fea1a8e5465cc63ad549f94aea3cf9bba
leigh966 <leigh966@hotmail.com>
2023-10-15 15:30:58 +0100

    various asthetic improvements

--- DarkSoulsSaver/Form1.Designer.cs
+++ DarkSoulsSaver/Form1.Designer.cs
@@ -35,28 +35,32 @@
             label1 = new Label();
             txtDescription = new TextBox();
             btnDelete = new Button();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 20;
-            listBox1.Location = new Point(14, 17);
+            listBox1.Location = new Point(6, 27);
             listBox1.Margin = new Padding(3, 4, 3, 4);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(593, 364);
+            listBox1.Size = new Size(593, 284);
             listBox1.TabIndex = 0;
             listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // btnLoad
             // 
-            btnLoad.Location = new Point(614, 68);
+            btnLoad.Location = new Point(8, 27);
             btnLoad.Margin = new Padding(3, 4, 3, 4);
             btnLoad.Name = "btnLoad";
-            btnLoad.Size = new Size(224, 44);
+            btnLoad.Size = new Size(210, 44);
             btnLoad.TabIndex = 1;
-            btnLoad.Text = "Load Backup";
+            btnLoad.Text = "Load";
             btnLoad.UseVisualStyleBackColor = true;
             btnLoad.Click += btnLoad_Click;
             // 
@@ -65,21 +69,21 @@
             groupBox1.Controls.Add(btnCreate);
             groupBox1.Controls.Add(label1);
             groupBox1.Controls.Add(txtDescription);
-            groupBox1.Location = new Point(614, 140);
+            groupBox1.Location = new Point(615, 152);
             groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
             groupBox1.Padding = new Padding(3, 4, 3, 4);
-            groupBox1.Size = new Size(224, 244);
+            groupBox1.Size = new Size(224, 174);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             groupBox1.Text = "Create Backup";
             // 
             // btnCreate
             // 
-            btnCreate.Location = new Point(42, 171);
+            btnCreate.Location = new Point(37, 109);
             btnCreate.Margin = new Padding(3, 4, 3, 4);
             btnCreate.Name = "btnCreate";
-            btnCreate.Size = new Size(144, 65);
+            btnCreate.Size = new Size(144, 55);
             btnCreate.TabIndex = 2;
             btnCreate.Text = "Create";
             btnCreate.UseVisualStyleBackColor = true;
@@ -88,7 +92,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(7, 25);
+            label1.Location = new Point(7, 24);
             label1.Name = "label1";
             label1.Size = new Size(85, 20);
             label1.TabIndex = 1;
@@ -96,42 +100,65 @@
             // 
             // txtDescription
             // 
-            txtDescription.Location = new Point(7, 49);
+            txtDescription.Location = new Point(8, 48);
             txtDescription.Margin = new Padding(3, 4, 3, 4);
+            txtDescription.MaxLength = 40;
             txtDescription.Multiline = true;
             txtDescription.Name = "txtDescription";
-            txtDescription.Size = new Size(210, 100);
+            txtDescription.Size = new Size(210, 53);
             txtDescription.TabIndex = 0;
             // 
             // btnDelete
             // 
-            btnDelete.Location = new Point(614, 16);
+            btnDelete.Location = new Point(8, 79);
             btnDelete.Margin = new Padding(3, 4, 3, 4);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(224, 44);
+            btnDelete.Size = new Size(210, 44);
             btnDelete.TabIndex = 3;
-            btnDelete.Text = "Delete Backup";
+            btnDelete.Text = "Delete";
             btnDelete.UseVisualStyleBackColor = true;
             btnDelete.Click += btnDelete_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnLoad);
+            groupBox2.Controls.Add(btnDelete);
+            groupBox2.Location = new Point(615, 10);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(224, 135);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Selected Backup";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(listBox1);
+            groupBox3.Location = new Point(4, 10);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(605, 316);
+            groupBox3.TabIndex = 5;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Backups";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(851, 403);
-            Controls.Add(btnDelete);
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(851, 343);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Controls.Add(listBox1);
-            Controls.Add(btnLoad);
             Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
-            MaximumSize = new Size(869, 450);
-            MinimumSize = new Size(869, 450);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -144,5 +171,7 @@
         private Label label1;
         private TextBox txtDescription;
         private Button btnDelete;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
     }
 }
